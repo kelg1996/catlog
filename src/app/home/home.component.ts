fb91d7daf7a365840d3a79000d92e861c6a76a6c
kelg1996 <46920620+kelg1996@users.noreply.github.com>
2023-01-25 18:47:53 -0500

    Add files via upload

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,45 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {TableColumns, TableData} from "../table/table.types";
-import { faker } from '@faker-js/faker';
+
 import {Observable, Subject} from "rxjs";
 import {HttpClient} from "@angular/common/http";
 import {ClrDatagridFilterInterface} from "@clr/angular";
-import {AgefilterComponent} from "../agefilter/agefilter.component";
-import {DatefilterComponent} from "../datefilter/datefilter.component";
+import {DateFilterComponent} from "./date-filter/date-filter.component";
 
 
-interface Todo {
+
+export interface Todo {
   userId: number;
   id: number;
   title: string;
   completed: boolean;
 }
 
+export interface User {
+  username: string;
+  email: string;
+  age: number;
+  weight: number;
+  birthDate: string;
+}
 
 
-export  class AgeFilter implements ClrDatagridFilterInterface<Todo> {
+export class TitleFilter implements ClrDatagridFilterInterface<Todo> {
   changes = new Subject<any>();
-  public minAge!: number;
-  public  maxAge!: number;
-  accepts(person: Todo): boolean {
-    return true
+  state = {
+    title: ''
   }
 
-  isActive(): boolean {
-    return false
+  public accepts(item: Todo): boolean {
+    return item.title.includes(this.state.title);
+  }
+
+  public isActive(): boolean {
+
+    return this.state.title.length > 0;
   }
 
 }
 
-export class BirthdayFilter implements ClrDatagridFilterInterface<Todo> {
-  changes = new Subject<any>;
-  value = '';
-  accepts(item: Todo): boolean {
-    return true
+export class DateFilter implements ClrDatagridFilterInterface<User> {
+  changes = new Subject<any>();
+  state = {
+    minDate: '',
+    maxDate: ''
   }
-
-  isActive(): boolean {
-    return false
+  public accepts(item: User): boolean {
+    let accept = true;
+    if (this.state.minDate) {
+      accept = accept && new Date(item.birthDate).getTime() >= new Date(this.state.minDate).getTime();
+    }
+    if (this.state.maxDate) {
+      accept = accept && new Date(item.birthDate).getTime() <= new Date(this.state.maxDate).getTime();
+    }
+    return accept;
+  }
+  public isActive(): boolean {
+    return this.state.minDate !== '' || this.state.maxDate !== '';
   }
 }
 
@@ -49,38 +68,41 @@ export class BirthdayFilter implements ClrDatagridFilterInterface<Todo> {
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  columns: TableColumns<Todo> = [
+  columns: TableColumns<User> = [
     {
-      title: 'User ID',
-      key: 'userId'
+      title: 'Username',
+      key: 'username',
     },
     {
-      title: 'ID',
-      key: 'id',
+      title: 'Age',
+      key: 'age'
     },
     {
-      title: 'Title',
-      key: 'title',
+      title: 'Weight',
+      key: 'weight'
     },
     {
-      title: 'Status',
-      key: 'completed',
-      render: (todo: Todo) => {
-        return todo.completed ? 'Finished' : 'Not Yet'
+      title: 'Birthday',
+      key: "birthDate",
+      filter: {
+        component: DateFilterComponent,
+        filter: new DateFilter()
       }
-    },
+    }
 
   ];
-  tableData: TableData<Todo> | Observable<TableData<Todo>> = [];
+  tableData: TableData<User> | Observable<TableData<User>> = [];
   page = 1;
   pageSize = 10;
   total = 1000;
-  constructor(private httpClient: HttpClient) { }
+
+  constructor(private httpClient: HttpClient) {
+  }
 
 
-  renderExpand(p: Todo) {
+  renderExpand(p: User) {
     return `
-      <pre class="bg-red">${p.title}</pre>
+      <pre class="bg-red">${p.username}</pre>
     `
   }
 
@@ -90,9 +112,9 @@ export class HomeComponent implements OnInit {
   }
 
   requestApi() {
-    this.httpClient.get<Todo[]>(`https://jsonplaceholder.typicode.com/todos/`)
+    this.httpClient.get<{users: User[]}>(`https://dummyjson.com/users`)
       .subscribe(data => {
-        this.tableData = data
+        this.tableData = data.users
       })
   }
 
